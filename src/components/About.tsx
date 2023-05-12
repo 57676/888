62f198d82aa17ad4689f45952cb1a7ade831fc03
57676 <128841884+57676@users.noreply.github.com>
2023-05-12 18:28:31 +0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,54 +1,5 @@
-export default () => (
-  <div text-sm text-slate-4 pt-2 ml-1 border-t border-dashed border-slate>
-    <span text-lg text-pink-6>关于</span>
-    {"  "}
-  
-    <a
-      border-b
-      border-slate
-      border-none
-      hover:border-dashed
-      href="httpz://mxnf.tk"
-      target="_blank">
-    
-    </a>{" "}
-   网
-    <strong>
-      {" "}
-      <a
-        border-b
-        border-slate
-        border-none
-        hover:border-dashed
-        href="http://mxnf.tk"
-        target="_blank">
-       
-      </a>{" "}
-    
-    </strong>
+import React from "react";
 
-    <strong></strong>
-    
-    <p>
-      <span text-lg text-pink-6>重要</span>{" "}
-      因为访问人数较多,免费秘钥经常失效不稳定,建议联系站长个位数获取独立账号+秘钥+高级网站!{" "}
-      <strong>免费秘钥失效后，可进群联系站长</strong>{" "}
-      
-    </p>
-    <p>
-      <span text-lg text-pink-6>
-        更新
-      </span>{" "}
-      由于站内访问量过大，免费密钥即将消耗完，且用且珍惜～如您有多余接口贡献可联系站长，可免费为您提供技术支持
-    </p>
-    <br></br>
-    <p class="flex flex-wrap items-end mt-1">
-     官网更多黑科技{" "}
-      <a href="" target="_blank">
-        <img src="https://img.shields.io/badge/-进入官网-fff?logo=Telegram" />
-      </a>{" "}
-    
-     <a href=""><img src="" width="2541" height="1600" alt=""/></a>
-    </p>
-  </div>
-);
+export default function About() {
+  return <div></div>;
+}
